4a62218d2d4d4d3fc1fd2902f42038e7403a52ea
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-05-14 19:20:53 +0000

    page navigation

---
+++ trunk/Conversus.TerminalView/Views/Terminal/HomePage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Conversus.TerminalView.Views.Terminal
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public static RoutedEvent LinkClickedEvent;
+        
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private NavigationService navService = null;
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            navService = NavigationService.GetNavigationService(this);
+        }
+
+        private void Border_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var navigationURI = string.Empty;
+            var borderSender = (Border)sender;
+
+            switch (borderSender.Name)
+            {
+                case "TakingLink" :
+                case "ApprovementLink" :
+                    navigationURI = "Views/Terminal/InputName.xaml";
+                    break;
+                case "HasPinLink" :
+                    navigationURI = "Views/Terminal/InputPIN.xaml";
+                    break;
+                case "ToAnotherDayLink":
+                    navigationURI = "http://www.google.ru";
+                    break;
+            }
+
+            navService.Navigate(new Uri(navigationURI, UriKind.RelativeOrAbsolute));
+        }
+
+
+    }
+}
