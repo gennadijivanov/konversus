f077010d1a59d8d970ee054daa2ef1dccb78b8c8
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-05-16 19:27:19 +0000

    InputName upper panel/Back button.

--- Conversus.TerminalView/Views/Terminal/InputName.xaml.cs
+++ Conversus.TerminalView/Views/Terminal/InputName.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Conversus.TerminalView.Views.Terminal
 {
@@ -22,6 +13,18 @@ namespace Conversus.TerminalView.Views.Terminal
         public Input_Name()
         {
             InitializeComponent();
+        }
+
+        private NavigationService navService = null;
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            navService = NavigationService.GetNavigationService(this);
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            navService.Navigate(new Uri("Views/Terminal/HomePage.xaml", UriKind.RelativeOrAbsolute));
         }
     }
 }
