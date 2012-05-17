fd4ae28b325fa1eb8d435205da2fc33061ae811e
gennadij.ivanov@gmail.com <gennadij.ivanov@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-05-17 04:47:25 +0000

    some test and logic

---
+++ trunk/Conversus.BusinessLogic.Test/ClientLogicTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Conversus.Core.DomainModel;
+using Conversus.Storage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Conversus.BusinessLogic.Impl;
+
+namespace Conversus.BusinessLogic.Test
+{
+    /// <summary>
+    /// Сводное описание для UnitTest1
+    /// </summary>
+    [TestClass]
+    public class ClientLogicTest
+    {
+        private const int pin = 12345;
+
+        /// <summary>
+        ///Получает или устанавливает контекст теста, в котором предоставляются
+        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
+        ///</summary>
+        public TestContext TestContext { get; set; }
+
+        #region Дополнительные атрибуты тестирования
+        //
+        // При написании тестов можно использовать следующие дополнительные атрибуты:
+        //
+        // ClassInitialize используется для выполнения кода до запуска первого теста в классе
+        [ClassInitialize]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            BusinessLogicInitializer.Initialize();
+            StorageLogicInitializer.Initialize();
+        }
+        //
+        // ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // TestInitialize используется для выполнения кода перед запуском каждого теста 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // TestCleanup используется для выполнения кода после завершения каждого теста
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        /// <summary>
+        ///Тест для Конструктор ClientLogic
+        ///</summary>
+        [TestMethod()]
+        public void ClientLogicConstructorTest()
+        {
+            IClientLogic target = BusinessLogicFactory.Instance.Get<IClientLogic>();
+            Assert.IsNotNull(target);
+        }
+
+        /// <summary>
+        ///Тест для ChangeStatus
+        ///</summary>
+        [TestMethod()]
+        public void ChangeStatusTest()
+        {
+            IClientLogic target = BusinessLogicFactory.Instance.Get<IClientLogic>();
+            Guid clientId = new Guid(); // TODO: инициализация подходящего значения
+            ClientStatus status = new ClientStatus(); // TODO: инициализация подходящего значения
+            target.ChangeStatus(clientId, status);
+            Assert.Inconclusive("Невозможно проверить метод, не возвращающий значение.");
+        }
+
+        /// <summary>
+        ///Тест для CreateForCommon
+        ///</summary>
+        [TestMethod()]
+        public void CreateForCommonTest()
+        {
+            IClientLogic target = BusinessLogicFactory.Instance.Get<IClientLogic>();
+            string name = string.Empty; // TODO: инициализация подходящего значения
+            QueueType queueType = new QueueType(); // TODO: инициализация подходящего значения
+            IClient expected = null; // TODO: инициализация подходящего значения
+            IClient actual;
+            actual = target.CreateForCommon(name, queueType);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Проверьте правильность этого метода теста.");
+        }
+
+        /// <summary>
+        ///Тест для CreateFromLotus
+        ///</summary>
+        [TestMethod()]
+        public void CreateFromLotusTest()
+        {
+            IClientLogic target = BusinessLogicFactory.Instance.Get<IClientLogic>();
+            const string name = "Vasya";
+            IClient actual = target.CreateFromLotus(name, pin);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(actual.Name, name);
+            Assert.AreEqual(actual.PIN, pin);
+        }
+
+        /// <summary>
+        ///Тест для GetClientByPin
+        ///</summary>
+        [TestMethod()]
+        public void GetClientByPinTest()
+        {
+            IClientLogic target = BusinessLogicFactory.Instance.Get<IClientLogic>();
+            IClient actual = target.GetClientByPin(pin);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(actual.PIN, pin);
+        }
+
+        /// <summary>
+        ///Тест для GetClients
+        ///</summary>
+        [TestMethod()]
+        public void GetClientsTest()
+        {
+            IClientLogic target = BusinessLogicFactory.Instance.Get<IClientLogic>();
+            QueueType queue = new QueueType(); // TODO: инициализация подходящего значения
+            ICollection<IClient> expected = null; // TODO: инициализация подходящего значения
+            ICollection<IClient> actual;
+            actual = target.GetClients(queue);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Проверьте правильность этого метода теста.");
+        }
+
+        /// <summary>
+        ///Тест для GetTicket
+        ///</summary>
+        [TestMethod()]
+        public void GetTicketTest()
+        {
+            IClientLogic target = BusinessLogicFactory.Instance.Get<IClientLogic>();
+            Guid clientId = new Guid(); // TODO: инициализация подходящего значения
+            string expected = string.Empty; // TODO: инициализация подходящего значения
+            string actual;
+            actual = target.GetTicket(clientId);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Проверьте правильность этого метода теста.");
+        }
+    }
+}
--- trunk/Conversus.BusinessLogic/Impl/ClientLogic.cs
+++ trunk/Conversus.BusinessLogic/Impl/ClientLogic.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Conversus.Core.DTO;
 using Conversus.Core.DomainModel;
+using Conversus.Core.Infrastructure.Repository;
 using Conversus.Impl.Repositories;
 using Conversus.Impl.Factories;
 
@@ -23,9 +24,13 @@ namespace Conversus.BusinessLogic.Impl
             throw new NotImplementedException();
         }
 
+        //TODO: create ticket factory
         public string GetTicket(Guid clientId)
         {
-            return "42";
+            // must create ticket by factory, set to object and return
+            
+            var client = RepositoryFactory.GetClientRepository().Get(clientId);
+            return client.Ticket;
         }
 
         public IClient GetClientByPin(int pin)
@@ -35,12 +40,15 @@ namespace Conversus.BusinessLogic.Impl
 
         public void ChangeStatus(Guid clientId, ClientStatus status)
         {
-            throw new NotImplementedException();
+            var client = RepositoryFactory.GetClientRepository().Get(clientId);
+            client.ChangeStatus(status);
         }
 
-        public ICollection<IClient> GetClients(QueueType queue)
+        public ICollection<IClient> GetClients(QueueType queueType)
         {
-            throw new NotImplementedException();
+            var queue = RepositoryFactory.GetQueueRepository()
+                .Get(new QueueFilterParameters {QueueType = queueType}).Single();
+            return RepositoryFactory.GetClientRepository().GetClients(queue.Id);
         }
 
         #endregion
