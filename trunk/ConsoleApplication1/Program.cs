07a5d7d7667159a429c6eac88eb403ecb8703ecb
gennadij.ivanov@gmail.com <gennadij.ivanov@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-05-18 06:13:10 +0000

    redis storage (dont work :( )

--- trunk/ConsoleApplication1/Program.cs
+++ trunk/ConsoleApplication1/Program.cs
@@ -1,6 +1,9 @@
-﻿using Conversus.BusinessLogic;
+﻿using System;
+using Conversus.BusinessLogic;
+using Conversus.Core.DTO;
 using Conversus.Core.DomainModel;
 using Conversus.Storage;
+using Conversus.Storage.Impl;
 
 namespace ConsoleApplication1
 {
@@ -8,11 +11,20 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            BusinessLogicInitializer.Initialize();
-            StorageLogicInitializer.Initialize();
+            //BusinessLogicInitializer.Initialize();
+            //StorageLogicInitializer.Initialize();
 
-            var clientLogic = BusinessLogicFactory.Instance.Get<IClientLogic>();
+            //var clientLogic = BusinessLogicFactory.Instance.Get<IClientLogic>();
             //clientLogic.CreateClient("Vasya", QueueType.Approvement, null);
+
+            RedisQueueStorage st = new RedisQueueStorage();
+
+            Guid id = Guid.NewGuid();
+
+            st.Create(new QueueData() {Id = id, Type = QueueType.Approvement});
+            st.Update(new QueueData() {Id = id, Type = QueueType.Taking});
+            var q = st.Get(id);
+            st.Delete(id);
         }
     }
 }
