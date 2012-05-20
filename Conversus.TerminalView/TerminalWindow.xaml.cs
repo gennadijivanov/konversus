099948e7af2e8c5cc23d3f4c9a66bb2f7b15e349
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-05-20 19:11:37 +0000

    Queue Plazma first look

--- Conversus.TerminalView/TerminalWindow.xaml.cs
+++ Conversus.TerminalView/TerminalWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TerminalView
 {
@@ -38,6 +27,13 @@ namespace TerminalView
                 this.WindowState = System.Windows.WindowState.Maximized;
                 this.WindowStyle = System.Windows.WindowStyle.None;
             }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TODO: queueBoard window
+            var queueBoard = new QueueBoardWindow();
+            queueBoard.Show();
         }
     }
 }
