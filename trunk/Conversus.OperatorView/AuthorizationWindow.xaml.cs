f29b6f0acbb09f17cd38f351c627e684f52c1d6d
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-05-21 19:37:10 +0000

    Operator view firs look/Authorization window

---
+++ trunk/Conversus.OperatorView/AuthorizationWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Conversus.OperatorView
+{
+    /// <summary>
+    /// Interaction logic for AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
