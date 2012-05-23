73520e7fb01e3213cbe7883145cded4f87051e66
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-05-23 19:34:33 +0000

    ConfirmPIN mockup done, AuthorizationWindow for operator done

--- trunk/Conversus.OperatorView/OperatorWindow.xaml.cs
+++ trunk/Conversus.OperatorView/OperatorWindow.xaml.cs
@@ -5,9 +5,9 @@ namespace Conversus.OperatorView
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class OperatorWindow : Window
     {
-        public MainWindow()
+        public OperatorWindow()
         {
             InitializeComponent();
         }
