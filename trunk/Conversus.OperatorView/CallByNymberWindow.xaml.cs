5b80f2f234a3618c4cba665083a23da8323c98dc
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-06-07 19:51:18 +0000

    callByNumberWidow complete

---
+++ trunk/Conversus.OperatorView/CallByNymberWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Conversus.OperatorView
+{
+    /// <summary>
+    /// Interaction logic for CallByNymberWindow.xaml
+    /// </summary>
+    public partial class CallByNymberWindow : Window
+    {
+        public CallByNymberWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)e.OriginalSource;
+
+            if (button.Name == "okButton")
+            {
+                //TODO: проверка на существование такого номера в базе
+            }
+            
+            this.Close();
+        }
+
+    }
+}
--- trunk/Conversus.OperatorView/OperatorWindow.xaml.cs
+++ trunk/Conversus.OperatorView/OperatorWindow.xaml.cs
@@ -8,6 +8,8 @@ namespace Conversus.OperatorView
     /// </summary>
     public partial class OperatorWindow : Window
     {
+        private const string CLEAN_CURRENT_VISITOR_TEXT = "---";
+
         public OperatorWindow()
         {
             InitializeComponent();
@@ -16,29 +18,44 @@ namespace Conversus.OperatorView
         private void Window_Click(object sender, RoutedEventArgs e)
         {
             Button button = (Button)e.OriginalSource;
-            
+
+            //TODO: Оживить timerLabel
             switch(button.Name)
             {
                 case "absenceButton":
+                    MessageBox.Show("Клиент " + currentVisitorTextBox.Text + "отмечена неявка");
                     break;
                 case "delayButton":
+                    MessageBox.Show("Клиент " + currentVisitorTextBox.Text + "добавлен в список отложенных");
                     break;
                 case "servedButton":
+                    MessageBox.Show("Обслуживание клиента " + currentVisitorTextBox.Text + "завершено");
+                    currentVisitorTextBox.Text = CLEAN_CURRENT_VISITOR_TEXT;
                     break;
                 case "repeatButton":
+                    //TODO: Вызвать текущего клиента ещё раз
                     break;
                 case "redirectButton":
+                    //TODO: Вывести окно с доступными очередями
                     break;
                 case "callVisitorButton":
+                    //TODO: Запросить нового в очереди
                     break;
                 case "callByNumberButton":
+                    var callByNumberWindow = new CallByNymberWindow();
+                    callByNumberWindow.Show();
                     break;
                 case "callByListButton":
+                    //TODO: Вывести окно со списком ожидающих
                     break;
                 case "pauseButton":
+                    //TODO: приостановить работу
+                    //не забыть спросить что сделать с клиентом,
+                    //если он ещё обслуживается
+                    MessageBox.Show("Система переведена в режим Перерыва");
                     break;
             }
-            MessageBox.Show(button.Name);
+            
         }
     }
 }
