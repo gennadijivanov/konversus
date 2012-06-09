d0636519eb8902bd8c042b5c6f8350688fad2abc
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-06-09 20:08:04 +0000

    RedirectWindow done

---
+++ trunk/Conversus.OperatorView/RedirectWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Conversus.OperatorView
+{
+    /// <summary>
+    /// Interaction logic for RedirectWindow.xaml
+    /// </summary>
+    public partial class RedirectWindow : Window
+    {
+        public RedirectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TODO: Запросить списоки для перенаправления
+            //либо передать в конструктор окна при открытии
+        }
+    }
+}
