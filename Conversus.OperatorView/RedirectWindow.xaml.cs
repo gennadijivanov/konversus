b98395bfcaee463dc694f7e765cd1c0e6370c458
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-06-10 11:45:54 +0000

    add redirectAndReturn button to redirect window / fix redirect logic

--- Conversus.OperatorView/RedirectWindow.xaml.cs
+++ Conversus.OperatorView/RedirectWindow.xaml.cs
@@ -15,8 +15,48 @@ namespace Conversus.OperatorView
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //TODO: Запросить списоки для перенаправления
+            //TODO: Запросить списки для перенаправления
             //либо передать в конструктор окна при открытии
+        }
+
+        private void Window_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)e.OriginalSource;
+
+            switch (button.Name)
+            {
+                case "redirectButton":
+                    redirectVisitor(false);
+                    break;
+                case "redirectAndReturnButton" :
+                    redirectVisitor(true);
+                    break;
+                case "cancelButton" :
+                    this.Close();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void redirectVisitor(bool IsNeedReturn)
+        {
+            if (employeeList.SelectedItem != null && queueList.SelectedItem != null)
+            {
+                //TODO: метод перенаправления передать туда селектед айтемы и отправить на нижний уровень
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать услугу и сотрудника для перенаправления посетителя");
+            }
+        }
+
+        private void list_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!redirectButton.IsEnabled || !redirectAndReturnButton.IsEnabled) 
+            {
+                redirectButton.IsEnabled = redirectAndReturnButton.IsEnabled = true;
+            }
         }
     }
 }
