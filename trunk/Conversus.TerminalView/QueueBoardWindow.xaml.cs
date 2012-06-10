c83937510fc31fb6285f9621995464a858ea807b
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-06-10 19:55:22 +0000

    video board add interactive directory read

--- trunk/Conversus.TerminalView/QueueBoardWindow.xaml.cs
+++ trunk/Conversus.TerminalView/QueueBoardWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.IO;
 
 namespace TerminalView
 {
@@ -11,6 +12,35 @@ namespace TerminalView
         public QueueBoardWindow()
         {
             InitializeComponent();
+        }
+
+        private string VIDEO_DIRECTORY_PATH = Path.GetDirectoryName("Content/video/");
+        private string[] fileEntries;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            checkeForModificationVideoDirectory();
+        }
+
+        private void checkeForModificationVideoDirectory()
+        {
+            //TODO: ПОЧЕМУ тут только 1 файл! Спросить у Гены
+            fileEntries = Directory.GetFiles(VIDEO_DIRECTORY_PATH);
+
+            if (fileEntries.Length > 0)
+            {
+                backgroundVideo.Source = new Uri(fileEntries[0], UriKind.Relative);
+                backgroundVideo.Play();
+            }
+            else
+            {
+                MessageBox.Show("Директория video пуста или отсутствует");
+            }
+        }
+
+        private void backgroundVideo_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            checkeForModificationVideoDirectory();
         }
     }
 }
