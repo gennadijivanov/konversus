97b018810668ebd0d311dfb5f578b3045c634aea
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-06-14 15:36:30 +0000

    add call sound / add second monitor detection for queue board / add print ticket view (need finish) / AdminWindow add grid and handlers for events

--- trunk/Conversus.AdminView/AdminWindow.xaml.cs
+++ trunk/Conversus.AdminView/AdminWindow.xaml.cs
@@ -12,5 +12,28 @@ namespace Conversus.AdminView
         {
             InitializeComponent();
         }
+
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: Вниз надо передать:
+            //nameTextBox.Text;
+            //loginTextBox.Text;
+            //passTextBox.Text;
+            //roleComboBox.SelectedIndex;
+        }
+
+        private void buildReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            //reportTypeComboBox.SelectedIndex;
+            //fromDateTime.Value;
+            //toDateTime.Value;
+            //на колбэк заполнить гридДату - reportGrid
+            //или через биндинг
+        }
+
+        private void exportButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: Тут хз. Надо посоветоваться с Геной.
+        }
     }
 }
