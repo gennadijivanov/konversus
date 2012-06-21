589991521f14821d6bcbc675a12318ff59ade15c
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-06-21 21:21:25 +0000

    dirty storage

---
+++ trunk/Conversus.Storage/Impl/ConversusDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conversus.Storage.Impl
+{
+    public class ConversusDataContext : conversusEntities
+    {
+        public ConversusDataContext() : base()
+        {
+            
+        }
+    }
+}
