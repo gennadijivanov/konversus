b36a3ebce4ca0694886fefb13053924041a36f8e
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-06-26 20:06:27 +0000

    user service interface

---
+++ trunk/Conversus.Service.Contract/UserInfo.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Conversus.Service.Contract
+{
+    [Serializable]
+    public class UserInfo
+    {
+        public Guid Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public QueueInfo Queue { get; set; }
+
+        public string CurrentWindow { get; set; }
+    }
+}
