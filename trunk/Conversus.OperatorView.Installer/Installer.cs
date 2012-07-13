ec5aa330e96f7e3da5a7663866374d99721f44b4
gennadij.ivanov@gmail.com <gennadij.ivanov@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-07-13 21:43:06 +0000

    Operator view installer

---
+++ trunk/Conversus.OperatorView.Installer/Installer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace Conversus.OperatorView.Installer
+{
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            //TODO: save to reg
+            var serverAddress = Context.Parameters["serverAddress"];
+            MessageBox.Show(serverAddress);
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+            //System.Diagnostics.Process.Start("http://www.microsoft.com");
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+    }
+}
--- trunk/Conversus.OperatorView/Properties/AssemblyInfo.cs
+++ trunk/Conversus.OperatorView/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Conversus.OperatorView")]
+[assembly: AssemblyTitle("Пульт оператора")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Conversus.OperatorView")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("Иванов, Воронин и партнеры")]
+[assembly: AssemblyProduct("Система управления очередью")]
+[assembly: AssemblyCopyright("Copyright © Иванов, Воронин и партнеры 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
