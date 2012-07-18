3368ea51b7c18227f7c92b8fb13c95bbdad48562
gennadij.ivanov@gmail.com <gennadij.ivanov@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-07-18 21:59:19 +0000

    host installer

---
+++ Conversus.Service.Host/HostService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceProcess;
+using Conversus.BusinessLogic;
+using Conversus.Core.Infrastructure;
+using Conversus.Service.Helpers;
+using Conversus.Service.Impl;
+using Conversus.Storage;
+
+namespace Conversus.Service.Host
+{
+    partial class HostService : ServiceBase
+    {
+        readonly Type _clientServiceType = typeof(ClientService);
+        readonly Type _queueServiceType = typeof(QueueService);
+        readonly Type _userServiceType = typeof(UserService);
+
+        private ServiceHost _clientHost;
+        private ServiceHost _queueHost;
+        private ServiceHost _userHost;
+
+        public HostService()
+        {
+            InitializeComponent();
+
+            StorageLogicInitializer.Initialize();
+            BusinessLogicInitializer.Initialize();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _clientHost = CreateServiceHost(_clientServiceType, Constants.Endpoints.ClientService);
+            _queueHost = CreateServiceHost(_queueServiceType, Constants.Endpoints.QueueService);
+            _userHost = CreateServiceHost(_userServiceType, Constants.Endpoints.UserService);
+        }
+
+        protected override void OnStop()
+        {
+            _clientHost.Close();
+            _queueHost.Close();
+            _userHost.Close();
+        }
+
+        private ServiceHost CreateServiceHost(Type serviceType, string endpoint)
+        {
+            var service = new ServiceHost(serviceType, new Uri(PropertyManager.Instance.ServiceHost + endpoint));
+            service.AddServiceEndpoint(
+                ServiceHelper.Instance.GetEndpoint(serviceType, endpoint, PropertyManager.Instance.ServiceHost));
+            service.Open();
+            return service;
+        }
+    }
+}
---
+++ Conversus.ServiceHost.Installer/Installer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Windows.Forms;
+using Conversus.Core.Infrastructure;
+
+
+namespace Conversus.ServiceHost.Installer
+{
+    [RunInstaller(true)]
+    public partial class Installer : InstallerWithTerminalBase
+    {
+        public Installer()
+        {
+            InitializeComponent();
+
+            //System.Diagnostics.Debugger.Launch();
+        }
+
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            var targetDir = Context.Parameters["targetDir"];
+
+            MessageBox.Show(targetDir);
+
+            //install host service
+        }
+    }
+}
