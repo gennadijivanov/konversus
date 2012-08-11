ffd0d64444eb8c07a893ab38c9ece6c1af47554e
voronin.job@gmail.com <voronin.job@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-08-11 14:49:52 +0000

    keygen generator done

---
+++ Conversus.Impl/EncryptionManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Conversus.Impl
+{
+    public class EncryptionManager
+    {
+        private const string content = "image";
+
+        public static string EncryptString(string Message, string Passphrase)
+        {
+            byte[] Results;
+            var UTF8 = new System.Text.UTF8Encoding();
+
+            var HashProvider = new MD5CryptoServiceProvider();
+            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(content));
+
+            var TDESAlgorithm = new TripleDESCryptoServiceProvider();
+
+            TDESAlgorithm.Key = TDESKey;
+            TDESAlgorithm.Mode = CipherMode.ECB;
+            TDESAlgorithm.Padding = PaddingMode.PKCS7;
+
+            byte[] DataToEncrypt = UTF8.GetBytes(Message);
+
+            try
+            {
+                ICryptoTransform Encryptor = TDESAlgorithm.CreateEncryptor();
+                Results = Encryptor.TransformFinalBlock(DataToEncrypt, 0, DataToEncrypt.Length);
+            }
+            finally
+            {
+                TDESAlgorithm.Clear();
+                HashProvider.Clear();
+            }
+            return Convert.ToBase64String(Results);
+        }
+    }
+}
