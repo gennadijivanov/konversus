aef6a6252da5647b9681871e8a65d5cc789d66ac
gennadij.ivanov@gmail.com <gennadij.ivanov@gmail.com@e0a02824-5d9d-43b2-93aa-0324bb088fbd>
2012-08-21 21:34:37 +0000

    setuper

--- Conversus.AdminView/App.xaml.cs
+++ Conversus.AdminView/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Conversus.Core.DomainModel;
+using Conversus.Core.Infrastructure;
 using Conversus.Service.Contract;
 using Conversus.Service.Helpers;
 
@@ -10,6 +12,14 @@ namespace Conversus.AdminView
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
 
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Logger.Log((Exception)e.ExceptionObject);
+        }
     }
 }
